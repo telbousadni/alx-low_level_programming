a8af6bfffb3a1bad87de1384e1124c3c1e883df8
telbousadni <taha.elbousadni@gmail.com>
2023-03-17 14:12:13 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
- * main: prints all single digit numbers of base 10 starting from 0
+ * main: prints numbers starting from 0 to 9
  *
  * Return: Always 0 
  */
@@ -14,5 +14,6 @@ int main(void)
 	{
 	printf("%d", i);
 	}
+	putchar('\n');
 return (0);
 }
