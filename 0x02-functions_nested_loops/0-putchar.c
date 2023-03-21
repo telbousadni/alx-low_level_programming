9b306cf721b26e63288b015f32326b719b3c76da
telbousadni <taha.elbousadni@gmail.com>
2023-03-21 16:04:59 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,6 +10,15 @@
 	 */
 	int _putchar(char c)
 	{
-		return (write(1, &c, 1));
-	}
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
 
+		return (0);
+	}
