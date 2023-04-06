1b826f14b3e60e2bf6f15aa6c7caa5d464397c9d
telbousadni <taha.elbousadni@gmail.com>
2023-04-06 14:32:03 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
- * _puts_recursion - function like puts()
- * @t: input
- * Return: Always 0
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
  */
-void _puts_recursion(char *t)
+void _puts_recursion(char *s)
 {
-	if (*t)
+	if (*s)
 	{
-		_putchar(*t);
-		_puts_recursion(t + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
+
 	else
 		_putchar('\n');
 }
