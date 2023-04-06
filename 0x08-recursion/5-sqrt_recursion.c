e5855eba1ff6c42fa0fdcb75b13e31655a468174
telbousadni <taha.elbousadni@gmail.com>
2023-04-06 16:44:50 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,33 +1,43 @@
 #include "main.h"
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: number which we want to calculate the square root
+ * _sqrt_recursion - Calculates the natural square root of a number.
+ * @n: The number to calculate the square root of.
  *
- * Return: square root of n
+ * Return: The natural square root of n, or -1 if n does not have a natural
+ * square root.
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if (n == 1 || n == 0)
+
+	if (n == 0 || n == 1)
 		return (n);
-	else
-		return (calculator_sqrt_recursion(n, 0));
+
+	return (_sqrt_recursion_calculator(n, 1, n));
 }
 /**
- * actual_sqrt_recursion - recurses to find the natural
- * square root of a number
- * @n: number to calculate the sqaure root of
- * @i: iterator
+ * _sqrt_recursion_helper - Helper function for _sqrt_recursion.
+ * @n: The number to calculate the square root of.
+ * @i: The starting point for the search.
+ * @end: The ending point for the search.
  *
- * Return: the resulting square root
+ * Return: The natural square root of n, or -1 if n does not have a natural
+ * square root.
  */
-int calculator_sqrt_recursion(int n, int i)
+	int _sqrt_recursion_calculator(int n, int i, int end)
 {
-	if (i * i > n)
+	int mid = (i + end) / 2;
+
+	if (i > end)
 		return (-1);
-	else if (i * i == n)
-		return (i);
+
+	if (mid * mid == n)
+		return (mid);
+
+	if (mid * mid > n)
+		return (_sqrt_recursion_calculator(n, i, mid - 1));
 	else
-		return (calculator_sqrt_recursion(n, i + 1));
+		return (_sqrt_recursion_calculator(n, mid + 1, end));
 }
+
