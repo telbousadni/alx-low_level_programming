af21c6b722318369151b0fc71e27643e4a084ef1
telbousadni <taha.elbousadni@gmail.com>
2023-04-13 18:29:30 -0700

    more arrays

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,13 +10,15 @@
  */
 int *array_range(int min, int max)
 {
-	int *ptr = malloc(sizeof(int) * size);
+	int *ptr;
 	int i, size;
 
 	if (min > max)
 		return (NULL);
 
 	size = max - min + 1;
+
+	ptr = malloc(sizeof(int) * size);
 
 	if (ptr == NULL)
 		return (NULL);
