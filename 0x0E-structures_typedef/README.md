08fe69d686879ee5a6f66c698d2b2afb8211b121
telbousadni <taha.elbousadni@gmail.com>
2023-04-17 19:23:04 -0700

    Dogs are my fav

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+free time i dream about you
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - name, age and owner
+ * @name: type char*
+ * @age: type int
+ * @owner: type char*
+ */
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+};
+#endif
