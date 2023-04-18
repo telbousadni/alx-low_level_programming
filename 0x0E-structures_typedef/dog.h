4b49261e0d85c6c7b9793003413cafce15b7fc9b
telbousadni <taha.elbousadni@gmail.com>
2023-04-17 19:33:00 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog - name, age and owner
  * @name: type char*
  * @age: type int
  * @owner: type char*
- *
- *
  */
 
 struct dog
@@ -16,5 +13,16 @@ struct dog
 	int age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 #endif
