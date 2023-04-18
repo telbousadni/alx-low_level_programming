e352f3f4ca91c3b1a27753e1f677dbba8663f4e6
telbousadni <taha.elbousadni@gmail.com>
2023-04-17 19:45:12 -0700

    ugh

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,12 +9,10 @@
  * @owner: owner to initialize
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
-{	
-	if(d == NULL)
-		d = malloc(sizeof(strugct dog));
-
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
 }
-
