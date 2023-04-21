db4b471d5f31775c60642c68d089ce4acb111a69
telbousadni <taha.elbousadni@gmail.com>
2023-04-20 19:39:15 -0700

    numbers print

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,18 +12,20 @@
 void print_numbers(const char *separator, const unsigned int n, ...);
 {
 	va_list nums;
-	unsigned int i;
+	unsigned int index;
 
 	va_start(nums, n);
 
-	for (i = 0; i < n; i++)
+	for (index = 0; index < n; index++)
 	{
 		printf("%d", va_arg(nums, int));
 
-		if (i != (n - 1) && separator != NULL)
+		if (index != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 
 	printf("\n");
+
 	va_end(nums);
+
 }
