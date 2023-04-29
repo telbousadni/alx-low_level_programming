344036d2622e866c16338f41fcfa3e990700aab9
telbousadni <taha.elbousadni@gmail.com>
2023-04-29 16:47:39 -0700

    Print list func

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all the elements of a listint_t list.
+ * @h : linked list argument of the function
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t n_nod++ = 0;
+
+	while (h->next != NULL)
+	{
+		printf("%d", h->n);
+		n_nod++;
+		h = h->next;
+	}
+	return (n_nod);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is a readme file, don't mind it
