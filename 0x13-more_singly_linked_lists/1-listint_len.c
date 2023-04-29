6158aef17756cda4d4bf37471ee2c0244afb640d
telbousadni <taha.elbousadni@gmail.com>
2023-04-29 16:55:19 -0700

    number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "limits.h"
+
+/**
+ * listint_len - function that returns the number of elements in a linked list
+ * @h: linked list argument of linstint_len function
+ *
+ * Return: the number of elements in a linked list *h
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t n_elem = 0;
+
+	while (h->next != NULL)
+	{
+		n_elem++;
+		h = h->next;
+	}
+	return (n_elem);
+}
