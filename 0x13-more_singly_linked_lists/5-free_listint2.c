be4382beb975736dec89ff341ab1047ce1dc43dd
telbousadni <taha.elbousadni@gmail.com>
2023-04-29 18:03:06 -0700

    freeyet

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,7 +2,7 @@
 
 /**
  * free_listint2 - frees a linked list
- * @head: pointer 
+ * @head: pointer
  */
 void free_listint2(listint_t **head)
 {
@@ -11,7 +11,7 @@ void free_listint2(listint_t **head)
 	if (head == NULL)
 		return;
 
-	while (*head)
+	while (*head != NULL)
 	{
 		del = (*head)->next;
 		free(*head);
