843bd589886725beee4a40ec71a3b3df78270db9
telbousadni <taha.elbousadni@gmail.com>
2023-04-29 18:00:11 -0700

    notfreeyet

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer 
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *del;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		del = (*head)->next;
+		free(*head);
+		*head = del;
+	}
+
+	*head = NULL;
+}
+
