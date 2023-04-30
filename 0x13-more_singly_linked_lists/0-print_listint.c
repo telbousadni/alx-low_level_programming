dc73cae4d892568cb84fdf3bb724d3e5e5f65568
telbousadni <taha.elbousadni@gmail.com>
2023-04-29 17:07:56 -0700

    print updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,9 +11,9 @@ size_t print_listint(const listint_t *h)
 {
 	size_t n_nod++ = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		printf("%d", h->n);
+		printf("%d\n", h->n);
 		n_nod++;
 		h = h->next;
 	}
