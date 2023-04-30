d8592bc3940c270758e8bcd8392ea60b96c5ef70
telbousadni <taha.elbousadni@gmail.com>
2023-04-29 18:33:46 -0700

    getn

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,10 +12,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 	listint_t *temp = head;
 
-	while (temp)
+	while (temp && i < index)
 	{
-		for (i = 0; i < index; i++)
 		temp = temp->next;
+		i++;
 	}
 
 	return (temp ? temp : NULL);
