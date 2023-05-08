5ac4801bf1b5bd5bc123d25fa72c1c36ab872a64
telbousadni <taha.elbousadni@gmail.com>
2023-05-08 14:28:35 -0700

    Start

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
+	buffer = malloc(sizeof(char) * letters);
 	t = read(fd, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, t);
 
