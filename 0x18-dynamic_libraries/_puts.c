463056c9cfa1beb2b6c15b65867fb5e9acd77734
telbousadni <taha.elbousadni@gmail.com>
2023-06-19 17:26:43 -0700

    rep

---
+++ 0x18-dynamic_libraries/_puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * _puts - a function that write the string
+ * @s: the given string
+*/
+void _puts(char *s)
+{
+	puts(s);
+}
